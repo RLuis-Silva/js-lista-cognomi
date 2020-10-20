4516258244e97a4ea51ca51dd8cee8cbfd8693a5
Luis Silva <s.ros.luis@gmail.com>
2020-10-20 09:24:31 +0200

    dimenticato di pushare es. non finito! =(

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-var arrayCognome = ["Bianchi", "Rossi", "Dunzioni", "Balsano", "Giorgini"];
+var arrayCognome = ["bianchi", "rossi", "dunzioni", "balsano", "giorgini"];
 var cognomeInput, risposta;
 var aggiungi = false;
+
 do{
     cognomeInput = prompt("inserisci il cognome: ");
     arrayCognome.push(cognomeInput);
@@ -16,5 +17,13 @@ do{
 
 console.log(arrayCognome);
 
+//ordine alfabetico
+console.log(arrayCognome.sort());
 
-// console.log(newArrayCognome.sort() );
+// cambio indice + 1
+//NON FUNZIONA
+// for(i = 0; i < arrayCognome.length; i++){
+//     newArrayCognome.push();
+// }
+
+// console.log(newArrayCognome);
